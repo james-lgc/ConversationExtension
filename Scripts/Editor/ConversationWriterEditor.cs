e0f27c7b52a935e40cbee0085fcd3d272b50c0b7
Unknown <jimbo@nicholl.org.uk>
2018-03-14 18:12:27 +0000

    Removed project specific text files, moved out of submodule.

--- Scripts/Editor/ConversationWriterEditor.cs
+++ Scripts/Editor/ConversationWriterEditor.cs
@@ -23,15 +23,6 @@ namespace DSA.Extensions.Conversations.Editor
 			ConversationWriter conversationWriter = (ConversationWriter)target;
 			serializedObject.Update();
 			DrawDefaultInspector();
-			System.Type type = conversationWriter.GetType();
-			BindingFlags flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
-			FieldInfo fi = type.GetField("conversationList", flags);
-			ConversationList list = (ConversationList)fi.GetValue(conversationWriter);
-			type = fi.FieldType;
-			fi = type.GetField("conversations", flags);
-			System.Collections.IList array = (System.Collections.IList)fi.GetValue(list);
-			Conversation conv = (Conversation)array[1];
-			EditorGUILayout.TextField(conv.Text);
 			if (GUILayout.Button("Edit"))
 			{
 				ConversationEditorWindow.Init();
